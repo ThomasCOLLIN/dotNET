e6ba383036bd60d8d85c932e0d3282366db0159b
Erika <moriceerika@gmail.com>
2013-05-26 21:48:34 +0200

    DataAccess
    - Modele.edmx
    => The objects are created

---
+++ Hopplr/DataAccess/Modele.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La génération de code par défaut est désactivée pour le modèle 'C:\Users\Erika\Documents\Epita\Cours\.NET - Apprentissage Environnement DotNet\Projet\dotNET\Hopplr\DataAccess\Modele.edmx'. 
+// Pour l'activer, modifiez la valeur du concepteur 'Stratégie de génération de code'.
+// Cette propriété est disponible dans la fenêtre Propriétés, lorsque le modèle est ouvert
+// dans le concepteur.
