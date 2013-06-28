ca976c81fcd1ab2926fbc0096e613948e21d4052
Thomas COLLIN <thmcollin@gmail.com>
2013-06-28 16:08:35 +0200

    flux rss avec tout

--- Hopplr/BusinessManagement/Feed.cs
+++ Hopplr/BusinessManagement/Feed.cs
@@ -12,14 +12,54 @@ namespace BusinessManagement
             foreach (DataAccess.T_Article article in articles)
             {
                 Dbo.RssArticle rssArticle = new Dbo.RssArticle();
-                rssArticle.Title = article.Text;
-                rssArticle.Content = "<img src=\"http://imgs.xkcd.com/comics/password_strength.png\">" + article.MediaUrl;
+                rssArticle.Title = "";
+                rssArticle.Content = GetMediaAsHtml((long) article.MediaTypeId, article.MediaUrl)
+                    + "<br>" + article.Text;
                 rssArticle.CreationDate = article.CreationDate;
 
                 rssArticles.Add(rssArticle);
             }
 
             return rssArticles;
+        }
+
+        private static string GetMediaAsHtml(long mediaType, string media)
+        {
+            switch (mediaType)
+            {
+                case ((long)Tools.MediaTypes.Image):
+                    return GetImageAsHtml(media);
+                case ((long)Tools.MediaTypes.Music):
+                    return GetSoundAsHtml(media);
+                case ((long)Tools.MediaTypes.Quote):
+                    return GetQuoteAsHtml(media);
+                case ((long)Tools.MediaTypes.Video):
+                    return GetVideoAsHtml(media);
+                default:
+                    return "";
+            }
+        }
+
+        private static string GetImageAsHtml(string image)
+        {
+            return "<img src=\"" + image + "\" alt=\"image\">";
+        }
+
+        private static string GetSoundAsHtml(string sound)
+        {
+            return "<video controls>"
+                + "<source src=\"" + sound + "\" type=\"audio/mpeg\">"
+                + "</video>";
+        }
+
+        private static string GetQuoteAsHtml(string quote)
+        {
+            return quote;
+        }
+
+        private static string GetVideoAsHtml(string videoId)
+        {
+            return "<iframe width=\"640\" height=\"385\" src=\"http://www.youtube.com/v/" + videoId + "\" frameborder=\"0\" allowfullscreen=\"true\"></iframe>";
         }
     }
 }
