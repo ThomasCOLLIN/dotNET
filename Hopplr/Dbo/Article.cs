085e0b4e45e9c2862c5806de96cc936555600850
goldsz_c <cloe.goldsztejn@gmail.com>
2013-06-29 00:39:34 +0200

    Creation d'un DBO pour Article

---
+++ Hopplr/Dbo/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dbo
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public long BlogId { get; set; }
+        public string MediaUrl { get; set; }
+        public Nullable<long> MediaTypeId { get; set; }
+        public string Text { get; set; }
+        public System.DateTime CreationDate { get; set; }
+
+        public List<Dbo.Tag> Tags { get; set; }
+        public List<Dbo.Comment> Comment { get; set; }
+    }
+}
---
+++ Hopplr/Dbo/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dbo
+{
+    public class Comment
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public long ArticleId { get; set; }
+        public string Content { get; set; }
+        public System.DateTime CreationDate { get; set; }
+    }
+}
